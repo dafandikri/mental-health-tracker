eb7a2b9c14f67e48c6559a9f3b21678f80a7174e
dafandikri <dafandikri@gmail.com>
2024-08-28 14:24:04 +0700

    Modified ALLOWED_HOSTS at settings.py

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d=pyskxl((%xh&p!dy7abwtgwkff5+k=&$!#(j4&l+b&$3(z=e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://pbp.cs.ui.ac.id/erdafa.andikri/mentalhealthtracker"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "erdafa-andikri-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
